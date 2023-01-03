094cce84a04c7d767a551911caeafebfa96d5d71
Bogdan Mierloiu <bogdy.musiclover@gmail.com>
2023-01-04 00:49:48 +0200

    first commit

---
+++ src/main/java/com/example/budget/Budget.java
@@ -0,0 +1,13 @@
+package com.example.budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Budget {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Budget.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/budget/BudgetTests.java
@@ -0,0 +1,13 @@
+package com.example.budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
